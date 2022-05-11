716cce5d73f41cc701b5894b161b2565e7301f20
Cryptic <willie@cyberdevs.co.za>
2022-05-11 21:23:16 +0200

    Tournament V1.5

--- Chaos.js
+++ Chaos.js
@@ -14,6 +14,7 @@ let m; //Moves
 var mx, my, ma, mp, ms, mct, mrn=0; //x,y,available,population, scout, tier, resnodes
 
 var en=[];
+var heatreq=14000000;
 
 const connection = new signalR.HubConnectionBuilder().withUrl(url).configureLogging(signalR.LogLevel.Information).build();
 
@@ -49,39 +50,42 @@ var nt;
 
 var mf, mw, mo, mh; //Food, Wood, Stone, heat
 var nf, nw, ns, nh; //Need Food, Wood, Stone, Heat
-var wf, ww, ws, wh; //Worker food, wood, stone, heat	
+var wf, ww, ws, wh; //Worker food, wood, stone, heat
+var tf, tw, ts; //total res harvested to rebalance the max storage
 
-var lv=0.16; //Livetime value of a unit
-			 //Calculated 10 Rounds = 3 / 5 wood + 1 food. meaning each unit has an upkeep of 0,16 resources per round.
-			 //Every Base gives a certain amount of res per round calculated at gamestart. 
-			 //This means the moment a unit upkeep is less than what the closest base with abundence is its no longer profitable to upkeep units.
-			 //At this stage I can stop campfires completely and use the units I have left to stockpile.
-			 //Since an end unit gives 25 points, keeping units alive for 160 rounds will give less points than upkeep. 
-			 //It also means that after Round 160 a unit alive is worth more than a unit dead. So might want to restart campfires after Round 160 to allow growth and maximize points as long as we have food it should be great.
-			 //That last spike in population can really mess my opponents around, 
-			 //Considered leaving this out for Round 1, but I really want that Golden Ticket. And getting the mining sequence the same as mine will be a nightmare if theres added updates. (Especially if my "available units" game is perfect.)
-
-var kill=false; //Kill when food becomes unsustainible
-//Dont farm while kill = true (so nodes can refill more)
-	//Wastes less population on harvesting dead points
-	//set minimum sustaining population (maybe 500) -> want to try and get population back to 20 000 for endgame. (for 500 000 points)
+var lv=0.16; //Livetime value of a unit (Absolutely no usecase in V1.5 but could be essential later on.)
 
 var sct=0;//Scout
 
-//generate store 30 rounds worth of res farming in array (4 for heat)
-var fa=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-	wa=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-	sa=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	ha=[0,0,0,0];
+//Cycles (Upkeep happens every 10 ticks, so all that matters is that I have an X amount of res dropping in every cycle. )
+	//Tracking Cycles also helps harvest as much res as possible from distant nodes to ease my own nodes. 
+	//Will do closest nodes for upkeep, growth and storage
+	//Will do furthest node for cycle preparations
+	//Aim to get the res between cycle 0.2 and 0.8 to avoid engine delays getting messy As early as possible so that the units are ready to go to the next cycle...
+
+var cf=[0,0,0,0,0,0,0],cw=[0,0,0,0,0,0,0],cs=[0,0,0,0,0,0,0];
+var cycle=0, ncycle;
+var minr,maxr;
 
 connection.on("ReceiveBotState", gameState => {
-   	w=gameState.world;
+   	
+	w=gameState.world;
 	b=gameState.bots;
 	p=gameState.populationTiers;
 	
 	r=w.currentTick;
+	cycle=Math.floor(r/10);
+	if(r%10==0){
+		cs.shift();cs.push(0);
+		cf.shift();cf.push(0);
+		cw.shift();cw.push(0);
+	}
+	
+	console.log("Cycle Cariables: food"+JSON.stringify(cf)+" wood"+JSON.stringify(cw)+" stone"+JSON.stringify(cs));
 	
 	console.log("Tick: "+r);
+	console.log("Cycle: "+cycle);
+	
 	if(r==1){
 		//Set up initial data
 		bi=gameState.botId;
@@ -100,13 +104,15 @@ connection.on("ReceiveBotState", gameState => {
 		s.sort(function(a,b){return a[3]-b[3]});
 		}
 	
+	console.log("Scout Towers Complete:"+JSON.stringify(s));
+	
+	
 	m={"playerId" : bi,"actions" : []} 
 	
 	for(i = b.length-1; i>=0; i--){
 			
 			if(b[i].id==bi){
 				ma=b[i].availableUnits;
-				ms=b[i].scoutingUnits;
 				mp=b[i].population;
 				mf=b[i].food;
 				mw=b[i].wood;
@@ -141,11 +147,11 @@ connection.on("ReceiveBotState", gameState => {
 						tn=w.map.nodes[i];
 						ds=bR(d(tn.position.x,tn.position.y,mx,my))+tn.workTime;
 						if(tn.type==1){
-							wn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
+							wn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits-tn.currentUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
 						} else if(tn.type==2){
-							fn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
+							fn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits-tn.currentUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
 						} else {
-							sn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
+							sn.push([tn.id,tn.position.x,tn.position.y,tn.maxUnits-tn.currentUnits,tn.reward,tn.amount,Math.round(tn.reward/ds * 100) / 100, ds, tn.currentUnits]);
 						}
 					}
 
@@ -159,28 +165,19 @@ connection.on("ReceiveBotState", gameState => {
 	}
 	
 	console.log("Resource Nodes available:"+mrn);
-	
 	for(i = w.map.nodes.length-1; i>=0; i--){
 						tn=w.map.nodes[i];
 						if(tn.type==1){
-							for(j=wn.length-1;j>=0;j--){if(wn[j][0]==tn.id){wn[j][3]=tn.maxUnits;wn[j][5]=tn.amount-(tn.currentUnits*tn.reward);wn[j][8]=tn.currentUnits;}}
+							for(j=wn.length-1;j>=0;j--){if(wn[j][0]==tn.id){wn[j][3]=tn.maxUnits-tn.currentUnits;wn[j][5]=tn.amount-(tn.currentUnits*tn.reward);wn[j][8]=tn.currentUnits;}}
 						} else if(tn.type==2){
-							for(j=fn.length-1;j>=0;j--){if(fn[j][0]==tn.id){fn[j][3]=tn.maxUnits;fn[j][5]=tn.amount-(tn.currentUnits*tn.reward);fn[j][8]=tn.currentUnits;}}
+							for(j=fn.length-1;j>=0;j--){if(fn[j][0]==tn.id){fn[j][3]=tn.maxUnits-tn.currentUnits;fn[j][5]=tn.amount-(tn.currentUnits*tn.reward);fn[j][8]=tn.currentUnits;}}
 						} else {
-							for(j=sn.length-1;j>=0;j--){if(sn[j][0]==tn.id){sn[j][3]=tn.maxUnits;sn[j][5]=tn.amount-(tn.currentUnits*tn.reward);sn[j][8]=tn.currentUnits;}}
+							for(j=sn.length-1;j>=0;j--){if(sn[j][0]==tn.id){sn[j][3]=tn.maxUnits-tn.currentUnits;sn[j][5]=tn.amount-(tn.currentUnits*tn.reward);sn[j][8]=tn.currentUnits;}}
 						}
 					}
 	
 	
-	fa.shift();fa.push(0);
-	wa.shift();wa.push(0);
-	sa.shift();sa.push(0);
-	ha.shift();ha.push(0);
-	 
-	//Lf last round population larger than this rounds population, kill all. (tracking bases must still be better)
-	//When farming, Add a resource cache and ignore closest res according to cache + 10% (for food) (Reason 10% will account for the farming)
-	
-	if(kill){console.log("Unsustainible population - turn off campfires completely (no more heat)");}
+	minr=0;maxr=2500;
 	
 	if(ma>0){
 		
@@ -190,101 +187,114 @@ connection.on("ReceiveBotState", gameState => {
 		
 		//Handle Upkeep / Just dont die
 		nf=nf+mp;
+		tf=tf+mp;
 		nh=nh+mp;
+		
 		//nw=nw+Math.floor(mp*0.5)+1;
 		nw=nw+1;
-		if(mct>0){ns=ns+1;}//Ignore stone for tierre 1
+		tw=tw+1;
+		//if(mct>0){ns=ns+1;}//Ignore stone for tierre 1
 		//ns=ns+Math.floor(mp*0.1)+1;
-		if(nh>0){nw=nw+(Math.ceil(nh/5)*3);}
+		if(nh>0){nw=nw+(Math.ceil(nh/5)*3);
+				tw=tw+(Math.ceil(nh/5)*3);
+				}
 		
-		console.log("Needs after upkeep calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
+		//console.log("Needs after upkeep calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns+", Constraints - Food: "+tf+", Wood: "+tw+", Stone: "+ts);
 		
 		wf=0; ww=0; wh=0; ws=0;
 		farm();
-		cut();
 		burn();
+		cut();
+		//console.log("Needs after upkeep Actions - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns+", Constraints - Food: "+tf+", Wood: "+tw+", Stone: "+ts);
 		
-		console.log("Needs after upkeep Actions - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-		
-		
-		//First Growth / Things are going well, lets multiply //Maybe Redundent considering my While loop
+		//Handle population growth res *2 (Prioritize Growth, enough time for rest later)
 		if(ma>0){
-			
-		nf=nf+Math.ceil(mp/1.2);
-		nh=nh+Math.ceil(mp/0.9);
-		if(nh>0){nw=nw+(Math.ceil(nh/5)*3);}
-		console.log("Needs after Max Growth calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
 		
-		wf=0; ww=0; wh=0; ws=0;
-		cut();
-		farm();
-		burn();
-		
-		console.log("Needs after Max Growth Actions - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-		
-		if(nw>0){cut();}
+			for(j=0;j<3;j++){
+				nf=nf+Math.ceil(mp/1.2);
+				nh=nh+Math.ceil(mp/0.9);
+				tf=tf+Math.ceil(mp/1.2);
+				
+				if(nh>0){nw=nw+(Math.ceil(nh/5)*3);
+						tw=tw+(Math.ceil(nh/5)*3);
+						}
+				
+				wf=0; ww=0; wh=0; ws=0;
+				cut();
+				farm();
+				burn();
+				cut();
+
+			}
 		}
 		
 		//Lets explore
 		if(s.length>0){scout();}
 		
-		//All good, Will need to advance soon
 		if(ma>0){
-			//Handle Next Tier Advancement, Do not advance to last Tier
 			
-			mct=mct+1;
-			if(mct>5){mct=5;}	
+		nw=nw+p[mct].tierMaxResources.wood-tw;
+		ns=ns+p[mct].tierMaxResources.stone-ts;
+		nf=nf+p[mct].tierMaxResources.food-tf;	
 			
-			
-			nf=nf+p[mct].tierResourceConstraints.food;
-			nw=nw+p[mct].tierResourceConstraints.wood;
-			ns=ns+p[mct].tierResourceConstraints.stone+5;
-			
-			console.log("Needs after Advancement calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-			
-			mine();
-			cut();
-			farm();
-			
-			console.log("Needs after Advancement Actions - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-		}
-		
-		
-		
-		while(ma>0&&mrn>0&&!kill){
-			
-		nf=nf+Math.ceil(mp/1.2);
-		nh=nh+Math.ceil(mp/0.9);
-		if(nh>0){nw=nw+(Math.ceil(nh/5)*4);}
-		console.log("Needs after Max Growth calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-		
+		//console.log("Needs after Max Storage calc - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns+", Constraints - Food: "+tf+", Wood: "+tw+", Stone: "+ts);
 		wf=0; ww=0; wh=0; ws=0;
+		
+		mine();
 		cut();
 		farm();
-		burn();
 		
-		console.log("Needs after Max Growth Actions - Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
-		
-		if(nw>0){cut();}
 		}
 		
-		//What, still going? lets just hurt the trees (Triggers for kill active)
+		if(ma>0&&heatreq>0){
+		   nh=Math.ceil(mw-p[mct+1].tierResourceConstraints.wood*3); 
+		   if(nh>0){nw=nw+(Math.ceil(nh/5)*3);}	
+			
+			wf=0; ww=0; wh=0; ws=0;
+			
+		   burn();
+		   cut();
+		   }
 		
-		//Worth wiping all stone here. Reason has to do with distance. 
-			//Could catch enemies off guard, 
+		//If still have units left, seek advancement cycle
+			//Calculate next 6 ticks *5%
+			//If population > advancement, allow farming
+			//Write slowly, 
+		for(i=1;i<=7;i++){
+			mp=Math.ceil(mp*1.05);
+			if(mp>=p[mct].maxPopulation){console.log("Will Advance in "+i+" cycles");
+										console.log("Will Advance in "+i+" cycles");
+										 console.log("Will Advance in "+i+" cycles");
+										 console.log("Will Advance in "+i+" cycles");
+										 console.log("Will Advance in "+i+" cycles");
+										 
+										 minr=(cycle+i)*10+2;
+										 console.log("New Minr:"+minr);
+										 
+										 nw=nw+p[mct+1].tierMaxResources.wood-tw;
+										 ns=ns+p[mct+1].tierMaxResources.stone-ts;
+										 nf=nf+p[mct+1].tierMaxResources.food-tf;
+										 
+										 console.log("Need Wood:"+nw+" Need Stone:"+ns+" Need Food:"+nf);
+										 
+										 wf=0; ww=0; wh=0; ws=0;
+										 
+										 mine();
+										 cut();
+										 farm();
+										 
+										 break;
+										}
+			}
 		
-		nw=nw+10000000; 
-		if(nw>0){cut();}
 		
-		//Add in Stone hoarding here... 
-			//still to do, allow harvest Stone 
-			//Test if I remove initial sustain function. 
-			//Set a turn where i will allow population Growth, Maybe Round 160, and allow distant food harvest here (ignore kill)
-		
-		//What about stone starve early on
-		
-		console.log("Have Units Left After:"+ma+" - Use them");
+		if(ma>0){
+			console.log("Have Units Left After:"+ma+" - Use them");
+			}	
 		}
+	
+	console.log("Heat Requirements: "+heatreq);
+	
 	
 	sct--;
 	
@@ -326,43 +336,53 @@ function bR(td){
     return (((((td>0)?td:(-td))%1)===0.5)?((0===(tr%2))?tr:(tr-1)):tr);
 };
 
+
 function calcNeeds(){
-			nf=mf*-1;
-			nh=mh*-1;
-			ns=mo*-1;
-			nw=mw*-1;
-			for(i=0;i<3;i++){nh=nh-ha[i];nw=nw-wa[i];}
+			nf=(mf*-1)-cf[0]-cf[1]-cf[2]-cf[3]-cf[4]-cf[5]-cf[6];
+			nh=(mh*-1);
+			ns=(mo*-1)-cs[0]-cs[1]-cs[2]-cs[3]-cs[4]-cs[5]-cs[6];
+			nw=(mw*-1)-cw[0]-cw[1]-cw[2]-cw[3]-cw[4]-cw[5]-cw[6];
+			
+			ts=0;tf=0;tw=0;
+	
 			console.log("Needs after Travel Food:"+nf+", Wood:"+nw+", Heat:"+nh+", Stone: "+ns);
 		}
 
 function farm(){
-	for(i = 0; i<fn.length; i++){if(nf<1||kill){break;}	
+	for(i = 0; i<fn.length; i++){if(nf<1){break;}
+		if(r+fn[i][7]<minr||r+fn[i][7]>maxr){continue;}
+		if(fn[i][3]==0||fn[i][5]==0){continue;}
 		if(fn[i][5]>nf){wf=Math.ceil(nf/fn[i][4]);}
 		else {wf=Math.ceil(fn[i][5]/fn[i][4]);}					 
 		if(ma<wf){wf=ma;}
 		if(fn[i][3]<wf){wf=fn[i][3];}					 
 		if(wf>0){m.actions.push({"type" : 3,"units" : wf,"id" : fn[i][0]});ma=ma-wf;
-							fa[fn[i][7]]=fa[fn[i][7]]+(wf*fn[i][4]);
-				 			fn[i][5]=fn[i][5]-(wf*fn[i][4]);
+							fn[i][5]=fn[i][5]-(wf*fn[i][4]);
 							nf=nf-(wf*fn[i][4]);
-							
-				if(fn[i][6]<lv){
-					kill=true;}
+				 			fn[i][3]=fn[i][3]-wf;
+				 			
+				 			ncycle=Math.floor((r+fn[i][7])/10)-cycle;
+				 			cf[ncycle]=cf[ncycle]+(wf*fn[i][4]);
 				}
-		
 	}
 }
 
+
 function cut(){
 	for(i = 0; i<wn.length; i++){if(nw<1){break;}
+		if(r+wn[i][7]<minr||r+wn[i][7]>maxr){continue;}
+		if(wn[i][3]==0||wn[i][5]==0){continue;}
 		if(wn[i][5]>nw){ww=Math.ceil(nw/wn[i][4]);}
 		else {ww=Math.ceil(wn[i][5]/wn[i][4]);}
 		if(ma<ww){ww=ma;}
 		if(wn[i][3]<ww){ww=wn[i][3];}
 		if(ww>0){m.actions.push({"type" : 4,"units" : ww,"id" : wn[i][0]});ma=ma-ww;
-							wa[wn[i][7]]=wa[wn[i][7]]+(ww*wn[i][4]);
 				 			wn[i][5]=wn[i][5]-(ww*wn[i][4]);
 							nw=nw-(ww*wn[i][4]);
+				 			wn[i][3]=wn[i][3]-ww;
+				 
+				 			ncycle=Math.floor((r+wn[i][7])/10)-cycle;
+				 			cw[ncycle]=cw[ncycle]+(ww*wn[i][4]);
 							}
 		
 	}
@@ -370,35 +390,51 @@ function cut(){
 
 function mine(){
 	for(i = 0; i<sn.length; i++){if(ns<1){break;}
+		if(r+sn[i][7]<minr||r+sn[i][7]>maxr){continue;}
+		if(sn[i][3]==0||sn[i][5]==0){continue;}
 		if(sn[i][5]>ns){ws=Math.ceil(ns/sn[i][4]);}
 		else {ws=Math.ceil(sn[i][5]/sn[i][4]);}
 		if(ma<ws){ws=ma;}
-		if(sn[i][3]<ws){ws=sn[i][3];}
+		if(sn[i][3]<ws){ws=sn[i][3];}					 
 		if(ws>0){m.actions.push({"type" : 2,"units" : ws,"id" : sn[i][0]});ma=ma-ws;
-							sa[sn[i][7]]=sa[sn[i][7]]+(ws*sn[i][4]);
 				 			sn[i][5]=sn[i][5]-(ws*sn[i][4]);
 							ns=ns-(ws*sn[i][4]);
+				 			sn[i][3]=sn[i][3]-ws;
+				 
+				 			ncycle=Math.floor((r+sn[i][7])/10)-cycle;
+				 			cs[ncycle]=cs[ncycle]+(ws*sn[i][4]);
+				 
 							}
 		
 	}
 }
 
 function burn(){
-	if(nh>0&&mw>=3&&ma>0&&!kill){
+	if(nh>0&&mw>=3&&ma>0&&r<2490&&heatreq>0){
 		wh=Math.ceil(nh/5);
 		if(wh*3>mw){wh=Math.floor(mw/3);}
 		if(ma<wh){wh=ma;}
 		m.actions.push({"type" : 5,"units" : wh,"id" : "00000000-0000-0000-0000-000000000000"});
 		ma=ma-wh;
-		ha[2]=ha[2]+(wh*5);
-		wa[2]=wa[2]-(wh*3);
-		nh=nh-ha[3];
-		nw=nw+(wh*3);
+		nh=nh-(wh*5);
+		heatreq=heatreq-(wh*5);
+		//Remember to account for Wood in here, 
 	}
 }
 
+//split upkeep array from Max Res Array
+	//Track both seperatelyso that one Array can be for upkeep and the other for Maxres farming.
+	//Also allow upkeep to farm Population advances ahead of time. So that my units can drop res back into my base the same tick I advanced (I will leave a population tolerence in here. ). 
+
 function scout() {
-	if(ms==0&&sct<1){
+	if(sct<1){
 				for(i = 0; i<s.length; i++){if(ma==0||i>1){break;}m.actions.push({"type" : 1,"units" : 1,"id" : s[i][0]});ma--;sct=s[i][3];}
 		} 
 }
+
+//Early game harvest some stone from enemy nodes. 
+	//Ignore last unreachable levelup... in endneeds... 
+
+//Handling Max Storage... 
+
+//Readjust, Find Max growth so i can use extra units to work on tiere advancements and stockpiles

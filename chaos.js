da3a07cf705f038c13394e72bde9673ba409dccb
Willie Theron <80902384+Cryptic-Gamer@users.noreply.github.com>
2022-08-25 12:10:43 +0200

    Update chaos.js

--- chaos.js
+++ chaos.js
@@ -1106,7 +1106,7 @@ function cut(){
 		if(wn[i][1]==0||wn[i][3]==0){continue;}
 		if(wn[i][3]>nw){ww=Math.ceil(nw/wn[i][2]);}
 		else {ww=Math.ceil(wn[i][3]/wn[i][2]);}	
-		if(ma>100&&mp>5000&&bt[wn[i][7]][wn[i]][0]=='T'){m.actions.push({"type" : 11,"units" : 10,"id" : tmWoodQ[i][2]});ma=ma-10;}
+		if(ma>100&&mp>5000&&bt[wn[i][7]][wn[i]][0]=='T'){m.actions.push({"type" : 11,"units" : 10,"id" : wn[i][0]});ma=ma-10;}
 		if(ma<ww){ww=ma;}
 		if(wn[i][1]<ww){ww=wn[i][1];}
 		if(ww>0){
